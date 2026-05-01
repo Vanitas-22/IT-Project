ea45f57f79b9ce9aa601145c4491e9388e499c33
you12387 <you469542@gmail.com>
2026-05-02 00:09:08 +0300

    shop page

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
 
 /*shop page js*/
-let products = [
-    {name: "T-shirt", price: 120, image: "images/T-shirt.jpg"},
-    {name: "iphone-12", price: 150, image: "images/iphone-12.jpg"},
-    {name: "Hoodie", price: 90, image: "images/Hoodie.jpg"},
-    {name: "Laptop-mac", price: 170, image: "images/Laptop-mac.jpg"},
-    {name: "Shoes", price: 50, image: "images/shoes.jpg"},
-    {name: "Watch", price: 100, image: "images/watch.jpg"},
-    {name: "Cap", price: 15, image: "images/cap.jpg"},
-    {name: "Jacket", price: 120, image: "images/jacket.jpg"},
-    {name: "Bag", price: 20, image: "images/bag.jpg"},
-    {name: "jeans", price: 80, image: "images/jeans.jpg"},
-    {name: "sunglasses", price: 5, image: "images/sunglasses.jpg"},
-    {name: "Headphones", price: 100, image: "images/Headphones.jpg"},
-    {name: "Soundcore", price: 110, image: "images/Soundcore.jpg"},
-    {name: "laptopstand", price: 20, image: "images/laptopstand.jpg"},
-    {name: "cases", price: 15, image: "images/cases.jpg"},
-];
 
 let cartCount = 0;
 
-let container = document.getElementById("products");
 let cartText = document.getElementById("cartCount");
 
-function showProducts() {
-    for (let i = 0; i < products.length; i++) {
-        container.innerHTML += `
-        <div class="product">
-            <img src="${products[i].image}">
-            <h3>${products[i].name}</h3>
-            <p>$${products[i].price}</p>
-            <button onclick="addToCart()">Add to Cart</button>
-        </div>
-        `;
-    }
-}
 
 function setTheme(theme) {
-    document.body.className = theme;
+  if(theme === 'dark') {
+    document.body.classList.add('dark-mode');
+   } else {
+  document.body.classList.remove('dark-mode');
+   }
     document.cookie = "userTheme=" + theme + ";max-age=" + (30*24*60*60) + ";path=/";
     }
 window.onload = function() {
@@ -49,13 +23,16 @@ window.onload = function() {
             savedTheme = c.substring(10);
        }
     }
+     if(savedTheme === 'dark'){
      document.body.className = savedTheme;
+}
 };
-function addToCart() {
+function addToCart(name, price) {
     cartCount++;
     cartText.innerHTML = cartCount;
+console.log(`Added: ${name} - $${price}`);
 }
-showProducts();
+
 
 /* Contact Script */
 
